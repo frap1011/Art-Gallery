a830e9ae174ed5dc605a5f90f2a873175b289f05
frap1011 <mokajesse@gmail.com>
2018-11-08 15:20:05 -0600

    test

---
+++ Display.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Display : MonoBehaviour {
+    [SerializeField]
+    private Art show;
+    [SerializeField]
+    public Light shine;
+    public bool reachable;
+
+	void Start () {
+
+        shine = GetComponentInChildren<Light>();
+        shine.intensity = 0f;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(reachable && show == null)
+        {
+            shine.intensity = 100f;
+        }
+        else
+        {
+            shine.intensity = 0f;
+        }
+
+        if(show == null)
+        {
+            show = GetComponentInChildren<Art>();
+        }
+		else
+        {
+            show.transform.position = shine.transform.position;
+            if(show.GetComponent<Rigidbody>() != null)
+            {
+                Destroy(show.GetComponent<Rigidbody>());
+            }
+
+
+        }
+	}
+
+}
