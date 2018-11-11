2050bce22fbc7b2c2a3b1688fa5f54aaefa13d2a
frap1011 <mokajesse@gmail.com>
2018-11-11 14:38:47 -0600

    Level Change

--- Scene_Control.cs
+++ Scene_Control.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Scene_Control : MonoBehaviour {
 
     List<Display> displays = new List<Display>();
+    int cond;
 
 	void Start () {
         foreach(GameObject get in SceneManager.GetActiveScene().GetRootGameObjects())
@@ -12,6 +13,7 @@ public class Scene_Control : MonoBehaviour {
             if(get.GetComponent<Display>() != null)
             {
                 displays.Add(get.GetComponent<Display>());
+                cond++;
             }
         }
         foreach(Display set in displays)
@@ -22,6 +24,24 @@ public class Scene_Control : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(complete())
+        {
+            Debug.Log("You completed the level");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        }
 	}
+
+    private bool complete()
+    {
+        int pick = 0;
+        foreach(Display check in displays)
+        {
+            if(check.right)
+            {
+                pick++;
+            }
+        }
+        return cond == pick;
+    }
 }
